80216c05ed012976be8db8d3323a8249c0f2396b
raffaez <rafaeleasouzas@gmail.com>
2023-01-04 17:00:09 -0300

    feat: fetch and show products from api

--- web/src/pages/productList/ProductList.tsx
+++ web/src/pages/productList/ProductList.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './ProductList.scss';
+import Product from '../../models/Product';
+import { getAll } from '../../services/service';
 
 export default function ProductList() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  async function getProduct() {
+    await getAll("/get", setProducts);
+    console.log(products);
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, [products.length]);
+  
+  const attribute = (type: string) => {
+    return {
+      'BK': 'Weight',
+      'DC': 'Size',
+      'FN': 'Dimensions'
+    }[type];
+  }
+
   return (
-    <div></div>
+    <div className='product-list'>
+      {
+        React.Children.toArray(
+          products.map((product: any) => {
+
+            return (
+              <div className='product'>
+                <div>
+                  <input type="checkbox" name="delete-checkbox" id="delete-checkbox" />
+                </div>
+                <div className='product-sku'>
+                  {product.sku}
+                </div>
+                <div className='product-name'>
+                  {product.name}
+                </div>
+                <div className='product-price'>
+                  {product.price} $
+                </div>
+                <div className='product-attribute'>
+                {attribute(product.type)}: {product.attribute}
+                </div>
+              </div>
+            )
+          })
+        )
+      }
+    </div>
   )
 }

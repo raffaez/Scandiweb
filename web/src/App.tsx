9c3603d6a45e644599710d72507208d6d955e396
raffaez <rafaeleasouzas@gmail.com>
2023-01-04 17:02:56 -0300

    feat: initiate toastify; implement list and add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 import Nav from './components/nav/Nav';
+import ProductAdd from './pages/productAdd/ProductAdd';
 import ProductList from './pages/productList/ProductList';
 
 function App() {
   return (
     <Router>
       <Nav />
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        />
       <div style={{ minHeight: '100vh' }}>
         <Routes>
           <Route path="/" element={<ProductList />} />
-          {/* <Route path="/add" element={<AddProduct />} /> */}
+          <Route path="/list" element={<ProductList />} />
+          <Route path="/add" element={<ProductAdd />} />
         </Routes>
       </div>
     </Router>

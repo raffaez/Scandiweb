640e54b29159230dc620c0e742e67bfaed643f3d
raffaez <rafaeleasouzas@gmail.com>
2023-01-04 17:01:24 -0300

    feat: initiate axios and implement get functions

---
+++ web/src/services/service.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import Product from '../models/Product';
+
+const api = axios.create({
+  baseURL: 'http://localhost:80/ecommerce/server/products'
+});
+
+export const getAll = async (route: string, setData: React.Dispatch<React.SetStateAction<Product[]>>) => {
+  const response = await api.get(route);
+  setData(response.data.response);
+}
+
+export const getByKey = async (route: string, key: string) => {
+  const response = await api.get(`${route}/${key}`);
+  return response.data.response === 'No record found!' ? true : false;
+}

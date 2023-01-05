e20adfda9488207b58d298631d8b02d9815fe254
raffaez <rafaeleasouzas@gmail.com>
2023-01-04 22:27:40 -0300

    refactor: remove toastify (unused)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,7 @@
 import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 
 import Nav from './components/nav/Nav';
 import ProductAdd from './pages/productAdd/ProductAdd';
@@ -13,18 +11,6 @@ function App() {
   return (
     <Router>
       <Nav />
-      <ToastContainer
-        position="top-center"
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover={false}
-        theme="light"
-        />
       <div style={{ minHeight: '100vh' }}>
         <Routes>
           <Route path="/" element={<ProductList />} />
